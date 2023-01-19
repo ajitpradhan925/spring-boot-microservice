2000dccf2eaf49a981880e63e7789b0b714ee980
Ajit Pradhan <ajitpradhan925@gmail.com>
2023-01-19 07:34:18 +0530

    RabbitMq Has Been Integrated

--- customer/src/main/java/com/amigoscode/customer/CustomerService.java
+++ customer/src/main/java/com/amigoscode/customer/CustomerService.java
@@ -1,8 +1,8 @@
 package com.amigoscode.customer;
 
+import com.amigoscode.amqp.RabbitMQMessageProducer;
 import com.amigoscode.clients.fraud.FraudCheckResponse;
 import com.amigoscode.clients.fraud.FraudClient;
-import com.amigoscode.clients.notification.NotificationClient;
 import com.amigoscode.clients.notification.NotificationRequest;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,11 +11,9 @@ import org.springframework.web.client.RestTemplate;
 @Service
 @AllArgsConstructor
 public class CustomerService {
-    private final RestTemplate restTemplate;
     private final CustomerRepository customerRepository;
     private final FraudClient fraudClient;
-    private final NotificationClient notificationClient;
-
+    private final RabbitMQMessageProducer rabbitMQMessageProducer;
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder()
                 .firstName(request.firstName())
@@ -36,15 +34,16 @@ public class CustomerService {
         if (fraudCheckResponse.isFraudster()) {
             throw new IllegalStateException("fraudster");
         }
-
-        // todo: send notification
-        notificationClient.sendNotification(
-              new NotificationRequest(
-                      customer.getId(),
-                      customer.getEmail(),
-                      String.format("Hi %s's, welcome to Amigoscode....",
-                              customer.getFirstName())
-              )
+        NotificationRequest notificationRequest = new NotificationRequest(
+                customer.getId(),
+                customer.getEmail(),
+                String.format("Hi %s's, welcome to Amigoscode....",
+                        customer.getFirstName())
+        );
+        rabbitMQMessageProducer.publish(
+                notificationRequest,
+                "internal.exchange",
+                "internal.notification.routing-key"
         );
     }
 }
--- notification/src/main/java/com/amigoscode/notification/NotificationApplication.java
+++ notification/src/main/java/com/amigoscode/notification/NotificationApplication.java
@@ -1,13 +1,32 @@
 package com.amigoscode.notification;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@SpringBootApplication
+@SpringBootApplication(
+        scanBasePackages = {
+                "com.amigoscode.notification",
+                "com.amigoscode.amqp"
+        }
+)
 @EnableEurekaClient
 public class NotificationApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationApplication.class, args);
     }
+
+//    @Bean
+//    CommandLineRunner commandLineRunner(
+//            RabbitMQMessageProducer producer,
+//            NotificationConfig notificationConfig
+//            ) {
+//            return args -> {
+//                producer.publish(
+//                        new Person("Ajit", "25"),
+//                        notificationConfig.getInternalExchange(),
+//                        notificationConfig.getInternalNotificationRoutingKey());
+//            };
+//    }
+//
+//    record Person(String name, String age){}
 }
